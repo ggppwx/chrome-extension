025d0792d715d34447fa0a0d7b49bb86ecfdc837
jingwei <sirgujingwei@gmail.com>
2017-08-31 01:11:06 +0100

    update

--- background.js
+++ background.js
@@ -22,6 +22,85 @@ chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
 });
 */
 
+var CURRENT_BROWERSING_URL = undefined;
+var CURRENT_TIMER_IN_SECONDS = 0; 
+var CURRENT_TIMER = new Timer("global");
+
+// timer class 
+function Timer(name) {
+  this.name = name;
+  this.reset();
+}
+
+Timer.prototype.reset = function() {
+  this.start_time_in_seconds = undefined;
+  this.timer_counter = 0;
+  this.interval_id = undefined;
+}
+
+Timer.prototype.start = function() {
+  if (this.start_time_in_seconds) {
+    console.log('the timer is running.')
+  } else {
+    this.start_time_in_seconds = Date.now()/1000;
+    this.timer_counter = 0;
+    this.interval_id = setInterval( () => { // we can use 'this'
+      console.log('tick');
+      this.timer_counter += 1;
+    }, 1000);
+    console.log('start ticking');
+  }
+};
+
+Timer.prototype.stop = function() {
+  if (this.interval_id) {
+    clearInterval(this.interval_id);
+  }
+  var result = {
+    "end_time_in_seconds" : this.start_time_in_seconds + this.timer_counter,
+    "count_in_seconds" : this.timer_counter
+  }
+  this.reset();
+  return result;
+};
+
+
+function startTimer(url) {
+  // if url is another, stop current timer. 
+
+
+  CURRENT_TIMER.start()
+}
+
+function stopTimer() {
+  CURRENT_TIMER.stop();
+}
+
+function getUrlTimerStatus(url) {
+
+  chrome.storage.sync.get(url, function(records) {
+    // Notify that we saved.
+    console.log('current status for url ' + url);
+    console.log(records);
+
+  });
+
+
+} 
+
+
+function saveUrlTimerStatus(url) {
+
+}
+
+
+
+// data storage 
+// data structre for timer
+// [
+//   { url , timers per day , daily limit }
+// ]
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
@@ -54,15 +133,16 @@ chrome.runtime.onMessage.addListener(
 
 
 
-
+// start 
 chrome.tabs.onActivated.addListener(function(evt){ 
 
   chrome.tabs.getSelected(null, function(tab) {
     // alert(tab.url);  //the URL you asked for in *THIS QUESTION*
     console.log(tab.url);
     // get timer for current url 
-
-
+    let url = new URL(sender.tab.url);
+    let hostname = url.origin;
+    startTimer(hostname);
 
 
   });
