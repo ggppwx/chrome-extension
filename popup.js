d0a7d7a5755f8b895d89b1f057f81087c27475dc
jingwei <sirgujingwei@gmail.com>
2017-09-07 23:34:31 +0100

    update

--- popup.js
+++ popup.js
@@ -1,19 +1,27 @@
-document.body.onload = function() {
+$(document).ready(function() {
   chrome.storage.sync.get(function(items) {
     if (!chrome.runtime.error) {
-      console.log(items);
-      
-      for( let k in items) {
-      	let url = k;
-      	let secs = items[k].total_time_in_seconds;
-      	let rowContent = '<tr>'
-      	rowContent += '<td>' + url + '</td>';
-      	rowContent += '<td>' + secs + '</td>';
-      	rowContent += '</tr>'
+        console.log(items);
+        
+        var urlList = []
+        for( let k in items) {
+            let url = k;
+            let secs = items[k].total_time_in_seconds;
+            urlList.push({url : url, totalSeconds: secs});
+        }
 
-      	$('#stat table').append(rowContent);
-      }
+        urlList.sort(function(a, b) { return b.totalSeconds - a.totalSeconds });
+
+        for( let i = 0; i <  urlList.length; ++i) {
+            let urlObj = urlList[i];
+            let rowContent = '<tr>'
+            rowContent += '<td>' + urlObj.url + '</td>';
+            rowContent += '<td>' + urlObj.totalSeconds + '</td>';
+            rowContent += '</tr>'
+
+            $('#stat table').append(rowContent);
+        }
       
     }
   });
-}
+});
